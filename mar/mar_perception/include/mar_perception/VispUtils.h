ac351dc9bb0571d257e155b05259636eb3dfbecf
davidfornas <davidfornas@gmail.com>
2014-05-20 17:18:20 +0200

    VispToTF improved

--- mar/mar_perception/include/mar_perception/VispUtils.h
+++ mar/mar_perception/include/mar_perception/VispUtils.h
@@ -5,25 +5,46 @@
 #include <visp/vpHomogeneousMatrix.h>
 #include <string>
 
+struct Frame{
+	tf::Transform pose;
+	std::string parent, child;
+	friend std::ostream& operator<<(std::ostream& out, Frame& x );
+};
 
 /** Tool to publish a vpHomogeneousMatrix on the TF tree */
 class VispToTF
 {
 
 	tf::TransformBroadcaster *broadcaster_;
-	tf::Transform pose_;
-	std::string parent_, child_;
+	std::map<std::string, Frame> frames_;
+
 
 public:
 
+    /** Create a new publisher with a frame to add to the TF tree */
+    VispToTF( vpHomogeneousMatrix sMs, std::string parent, std::string child );
+    /** Create a new empty publisher */
+    VispToTF();
 
-    /** Constructor from a VISP homoggeneous matrix and the parent and child ids of the frame */
-	VispToTF( vpHomogeneousMatrix sMs, std::string parent, std::string child );
-
-	void setTransform( vpHomogeneousMatrix sMs, std::string parent, std::string child  );
+    /** Add a frame to the publish list */
+	void addTransform( vpHomogeneousMatrix sMs, std::string parent, std::string child, std::string id  );
+	
+	/** Remove a frame from the publish list */
+	void removeTransform( std::string id  );
+	
+	
 	void publish();
+	void print();
 
    // ~VispToTF();
 };
+
+std::ostream& operator<<(std::ostream& out, Frame& x ) 
+{
+	  out << "Pose origin  [x: " << x.pose.getOrigin().x() << " y: " << x.pose.getOrigin().y() << " z: " << x.pose.getOrigin().z() << "] "
+	  << "Pose rotation [x:" << x.pose.getRotation().x() << " y:" << x.pose.getRotation().y() << " z:" << x.pose.getRotation().z() << " w:" << x.pose.getRotation().w() << "] "
+	  << std::endl << "Parent: " << x.parent << " Child: " << x.child << std::endl;
+	  return out;
+}
 
 #endif
--- mar/mar_perception/src/VispUtils.cpp
+++ mar/mar_perception/src/VispUtils.cpp
@@ -13,27 +13,66 @@
 
 VispToTF::VispToTF( vpHomogeneousMatrix sMs, std::string parent, std::string child){
 	  broadcaster_ = new tf::TransformBroadcaster();
-	  setTransform(sMs, parent, child);
+	  addTransform(sMs, parent, child, "0");
 }
 
-void VispToTF::setTransform( vpHomogeneousMatrix sMs, std::string parent, std::string child){
+VispToTF::VispToTF( ){
+	  broadcaster_ = new tf::TransformBroadcaster();
+}
 
-	  parent_=parent;
-	  child_=child;
+void VispToTF::addTransform( vpHomogeneousMatrix sMs, std::string parent, std::string child, std::string id){
+
+	  //TODO: It's possible to do further checks.
+      if(frames_.count(id)>0){
+		  std::cerr << "ID [" << id << "] already in use. It won't be added." << std::endl;
+		  return;		  
+	  }
+	  
+	  tf::Transform pose;
 
 	  vpTranslationVector trans;
 	  sMs.extract(trans);
 	  tf::Vector3 translation(trans[0],trans[1],trans[2]);
 
-	  vpQuaternionVector quat;
-	  sMs.extract(quat);
-	  tf::Quaternion rotation( quat.x(), quat.y(), quat.z(), quat.w());
-	  pose_.setOrigin(translation);   pose_.setRotation(rotation);
+	  vpQuaternionVector rot;
+	  sMs.extract(rot);
+	  tf::Quaternion rotation( rot.x(), rot.y(), rot.z(), rot.w());
+	  pose.setOrigin(translation);   pose.setRotation(rotation);
+	  
+	  Frame f; 
+	  f.pose=pose;
+	  f.parent=parent;
+	  f.child=child;
+	  
+	  frames_[id]=f;
+	  
+}
 
+void VispToTF::removeTransform( std::string id){
+	  if(frames_.count(id)<1){
+		  std::cerr << "Can't delete this item. ID [" << id << "] not found." << std::endl;
+		  return;		  
+	  }
+	  frames_.erase(id);
 }
 
 void VispToTF::publish(){
 
-    tf::StampedTransform transform(pose_, ros::Time::now(), parent_, child_);
-    broadcaster_->sendTransform(transform);
+	for( std::map<std::string, Frame>::iterator ii=frames_.begin(); ii!=frames_.end(); ++ii)
+		{
+			tf::StampedTransform transform((*ii).second.pose, ros::Time::now(), (*ii).second.parent, (*ii).second.child);
+			broadcaster_->sendTransform(transform);
+		}
+
 }
+
+void VispToTF::print(){
+
+	for( std::map<std::string, Frame>::iterator ii=frames_.begin(); ii!=frames_.end(); ++ii)
+		{
+			std::cout << "ID string: " << (*ii).first  << std::endl << 
+			"Frame -> " << (*ii).second << std::endl;
+		}
+
+}
+
