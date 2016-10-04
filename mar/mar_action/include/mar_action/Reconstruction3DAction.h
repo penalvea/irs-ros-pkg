d09fcc032091d792b48aa31789f1f0ee25551f8b
Toni Peñalver <penalve@uji.es>
2016-10-04 13:56:04 +0200

    some changes

--- mar/mar_action/include/mar_action/Reconstruction3DAction.h
+++ mar/mar_action/include/mar_action/Reconstruction3DAction.h
@@ -1,7 +1,7 @@
 #ifndef RECONSTRUCTION3DACTION_H
 #define RECONSTRUCTION3DACTION_H
 
-#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/PointCloud2.h>
 
 #include <mar_core/CAction.h>
 
@@ -17,119 +17,176 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/thread.hpp>
 
+#include <pcl_ros/point_cloud.h>
+#include <std_msgs/String.h>
+
 /** Maximum joint velocity to send to the arm */
-#define DEFAULT_MAX_JOINT_VELOCITY 0.02
+#define DEFAULT_MAX_JOINT_VELOCITY 0.1
 
 /** Stops the scanning when the position error of the joint with the maximum error is below POSITION_TOLERANCE radians */
 #define DEFAULT_POSITION_TOLERANCE 0.02
 
-class Reconstruction3DAction : public CAction {
-	typedef enum {SUCCESS, ERROR} output_t;
+class Reconstruction3DAction : public CAction
+{
+  typedef enum
+  {
+    SUCCESS, ERROR
+  } output_t;
 
-	//Pointers to perception classes
-	ArmPtr robot_;
-	ESMTrackingPtr tracker_;
-	std::vector<Reconstruction3DPtr> rec_;
-	//ArmLaserReconstruction3DEyePtr rec_eye_;
-	MotionEstimatorPtr mest_;
+  //Pointers to perception classes
+  ArmPtr robot_;
+  ESMTrackingPtr tracker_;
+  std::vector<Reconstruction3DPtr> rec_;
+  //ArmLaserReconstruction3DEyePtr rec_eye_;
+  MotionEstimatorPtr mest_;
 
-	vpColVector vp_scan_initial_posture_;
-	vpColVector vp_scan_final_posture_;
+  vpColVector vp_scan_initial_posture_;
+  vpColVector vp_scan_final_posture_;
 
-	double max_joint_velocity_;
-	double position_tolerance_;
+  double max_joint_velocity_;
+  double position_tolerance_;
 
-	bool fixed_base_; ///< If fixed base, do not perform tracking
-	bool offline_;	///< If offline, do not try to move the robot arm
-	bool done_;
-	bool publish_point_cloud_;
+  bool fixed_base_; ///< If fixed base, do not perform tracking
+  bool offline_;	///< If offline, do not try to move the robot arm
+  bool done_;
+  bool publish_point_cloud_;
 
-	ros::Publisher point_cloud_pub_;
-	ros::NodeHandle nh_;
+  ros::Publisher point_cloud_pub_;
+  ros::NodeHandle nh_;
+  ros::Subscriber benchmark_sub_;
 
 public:
-	Reconstruction3DAction( ArmPtr robot, ESMTrackingPtr tracker, MotionEstimatorPtr mest )
-	: CAction(), robot_(robot), tracker_(tracker), mest_(mest), max_joint_velocity_(DEFAULT_MAX_JOINT_VELOCITY), position_tolerance_(DEFAULT_POSITION_TOLERANCE),
-	  fixed_base_(false), offline_(false) {
-		point_cloud_pub_=nh_.advertise<sensor_msgs::PointCloud>("LaserReconstructionPointCloud", 1000);
-	}
+  Reconstruction3DAction(ArmPtr robot, ESMTrackingPtr tracker, MotionEstimatorPtr mest) :
+      CAction(), robot_(robot), tracker_(tracker), mest_(mest), max_joint_velocity_(DEFAULT_MAX_JOINT_VELOCITY), position_tolerance_(
+          DEFAULT_POSITION_TOLERANCE), fixed_base_(false), offline_(false), done_(false), publish_point_cloud_(false)
+  {
+    point_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("LaserReconstructionPointCloud", 1000);
+    //benchmark_sub_=nh_.subscribe("/BenchmarkInfo", 100, Reconstruction3DAction::benchmarkCallback);
+  }
 
-	Reconstruction3DAction()
-	: CAction(), max_joint_velocity_(DEFAULT_MAX_JOINT_VELOCITY), position_tolerance_(DEFAULT_POSITION_TOLERANCE),
-	  fixed_base_(true), offline_(true) {
-		point_cloud_pub_=nh_.advertise<sensor_msgs::PointCloud>("LaserReconstructionPointCloud", 1000);
-	}
+  Reconstruction3DAction() :
+      CAction(), max_joint_velocity_(DEFAULT_MAX_JOINT_VELOCITY), position_tolerance_(DEFAULT_POSITION_TOLERANCE), fixed_base_(
+          true), offline_(true), done_(false), publish_point_cloud_(false)
+  {
+    point_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("LaserReconstructionPointCloud", 1000);
+    //benchmark_sub_=nh_.subscribe("/BenchmarkInfo", 100, Reconstruction3DAction::benchmarkCallback);
+  }
 
-	Reconstruction3DAction( ESMTrackingPtr tracker, MotionEstimatorPtr mest )
-	: CAction(), tracker_(tracker), mest_(mest), max_joint_velocity_(DEFAULT_MAX_JOINT_VELOCITY), position_tolerance_(DEFAULT_POSITION_TOLERANCE),
-	  fixed_base_(false), offline_(true) {
-		point_cloud_pub_=nh_.advertise<sensor_msgs::PointCloud>("LaserReconstructionPointCloud", 1000);
-	}
+  Reconstruction3DAction(ESMTrackingPtr tracker, MotionEstimatorPtr mest) :
+      CAction(), tracker_(tracker), mest_(mest), max_joint_velocity_(DEFAULT_MAX_JOINT_VELOCITY), position_tolerance_(
+          DEFAULT_POSITION_TOLERANCE), fixed_base_(false), offline_(true), done_(false), publish_point_cloud_(false)
+  {
+    point_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("LaserReconstructionPointCloud", 1000);
+    //benchmark_sub_=nh_.subscribe("/BenchmarkInfo", 100, Reconstruction3DAction::benchmarkCallback);
+  }
 
-	Reconstruction3DAction( ArmPtr robot, std::vector<Reconstruction3DPtr> rec )
-	: CAction(), robot_(robot), rec_(rec), max_joint_velocity_(DEFAULT_MAX_JOINT_VELOCITY), position_tolerance_(DEFAULT_POSITION_TOLERANCE),
-	  fixed_base_(true), offline_(false) {
-		point_cloud_pub_=nh_.advertise<sensor_msgs::PointCloud>("LaserReconstructionPointCloud", 1000);
-	}
-	static void threadReconstruction(Reconstruction3DPtr rec, Reconstruction3DAction *act){
-		//int contador=0;
-		int cont_points=0;
-		while(ros::ok() && !act->done_){
+  Reconstruction3DAction(ArmPtr robot, std::vector<Reconstruction3DPtr> rec) :
+      CAction(), robot_(robot), rec_(rec), max_joint_velocity_(DEFAULT_MAX_JOINT_VELOCITY), position_tolerance_(
+          DEFAULT_POSITION_TOLERANCE), fixed_base_(true), offline_(false), done_(false), publish_point_cloud_(false)
+  {
+    point_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("LaserReconstructionPointCloud", 1000);
+    benchmark_sub_ = nh_.subscribe<std_msgs::String>("/BenchmarkInfo", 100, &Reconstruction3DAction::benchmarkCallback,
+                                                     this);
+  }
+  static void threadReconstruction(Reconstruction3DPtr rec, Reconstruction3DAction *act)
+  {
+    //int contador=0;
+    int cont_points = 0;
+    while (ros::ok() && !act->done_)
+    {
 
-			rec->perceive();
-			ros::spinOnce();
-		//	contador++;
-			if(act->publish_point_cloud_){
-				if(cont_points!=rec->points3d.size()){
-					sensor_msgs::PointCloud msg;
-					for(int i=cont_points; i<rec->points3d.size(); i++){
-						geometry_msgs::Point32 point;
-						point.x=rec->points3d[i][0];
-						point.y=rec->points3d[i][1];
-						point.z=rec->points3d[i][2];
-						msg.points.push_back(point);
-					}
-					cont_points=rec->points3d.size();
-					msg.header.stamp=ros::Time::now();
-					msg.header.frame_id="girona500/kinematic_base";
-					act->point_cloud_pub_.publish(msg);
-					ros::spinOnce();
-				}
-			}
-		}
-		//std::cout<<"Puntos: "<<rec->getCloud()->size()<<std::endl;
-		//std::cout<<"Contador thread"<<contador<<std::endl;
-		}
+      rec->perceive();
+      ros::spinOnce();
+      //	contador++;
+      if (act->publish_point_cloud_)
+      {
+        if (cont_points != rec->points3d.size())
+        {
 
-	boost::thread createThread(Reconstruction3DPtr rec){
-		return boost::thread (&Reconstruction3DAction::threadReconstruction, rec, this);
-	}
+          sensor_msgs::PointCloud2 msg;
+          /*for(int i=cont_points; i<rec->points3d.size(); i++){
+           geometry_msgs::Point32 point;
+           point.x=rec->points3d[i][0];
+           point.y=rec->points3d[i][1];
+           point.z=rec->points3d[i][2];
+           msg.points.push_back(point);
+           }*/
+          pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+          cloud = rec->getLimitedCloud(cont_points, rec->points3d.size());
+          cont_points = rec->points3d.size();
 
-	int doAction();	///< Perform the action
-	
-	virtual void draw();
+          //act->point_cloud_pub_.publish(*cloud);
+          pcl::toROSMsg(*cloud, msg);
+          msg.header.stamp = ros::Time::now();
+          msg.header.frame_id = "girona500/kinematic_base";
+          act->point_cloud_pub_.publish(msg);
+          ros::spinOnce();
+        }
+      }
+    }
+    //std::cout<<"Puntos: "<<rec->getCloud()->size()<<std::endl;
+    //std::cout<<"Contador thread"<<contador<<std::endl;
+  }
 
-	/** Set the joint values where to start the scan */
-	void setInitialPosture(vpColVector v) {vp_scan_initial_posture_=v;}
+  boost::thread createThread(Reconstruction3DPtr rec)
+  {
+    return boost::thread(&Reconstruction3DAction::threadReconstruction, rec, this);
+  }
 
-	/** Set the joint values where to finish the scan */
-	void setFinalPosture(vpColVector v) {vp_scan_final_posture_=v;}
+  int doAction();	///< Perform the action
 
-	void setMaxJointVelocity(double v) {max_joint_velocity_=v;}
-	void setTolerance(double t) {position_tolerance_=t;}
+  virtual void draw();
 
-	/** If set to true, the action does not perform tracking */
-	void setFixedBase(bool flag) {fixed_base_=flag;}
+  /** Set the joint values where to start the scan */
+  void setInitialPosture(vpColVector v)
+  {
+    vp_scan_initial_posture_ = v;
+  }
 
-	/** If set to true, the action does not try to access the robot arm */
-	void setOffline(bool flag) {offline_=flag;}
+  /** Set the joint values where to finish the scan */
+  void setFinalPosture(vpColVector v)
+  {
+    vp_scan_final_posture_ = v;
+  }
 
-	/** If set to true, the point cloud is published during the reconstruction */
-	void setPointCloudPublished(bool flag){publish_point_cloud_=flag;}
+  void setMaxJointVelocity(double v)
+  {
+    max_joint_velocity_ = v;
+  }
+  void setTolerance(double t)
+  {
+    position_tolerance_ = t;
+  }
 
-	void addReconstruction3D(Reconstruction3DPtr rec){ rec_.push_back(rec);}
+  /** If set to true, the action does not perform tracking */
+  void setFixedBase(bool flag)
+  {
+    fixed_base_ = flag;
+  }
 
-	virtual ~Reconstruction3DAction() {};
+  /** If set to true, the action does not try to access the robot arm */
+  void setOffline(bool flag)
+  {
+    offline_ = flag;
+  }
+
+  /** If set to true, the point cloud is published during the reconstruction */
+  void setPointCloudPublished(bool flag)
+  {
+    publish_point_cloud_ = flag;
+  }
+
+  void addReconstruction3D(Reconstruction3DPtr rec)
+  {
+    rec_.push_back(rec);
+  }
+  void benchmarkCallback(const std_msgs::String::ConstPtr &msg);
+
+  virtual ~Reconstruction3DAction()
+  {
+  }
+  ;
+
 };
 
 typedef boost::shared_ptr<Reconstruction3DAction> Reconstruction3DActionPtr;
--- mar/mar_action/src/Reconstruction3DAction.cpp
+++ mar/mar_action/src/Reconstruction3DAction.cpp
@@ -1,7 +1,6 @@
 #include <mar_action/Reconstruction3DAction.h>
 #include <boost/thread.hpp>
 
-
 /** Performs the logic necessary for 3D reconstruction using a laser mounted on an arm
  *  If not offline, the arm is initialized and moved to the starting configuration. It is then moved with joint velocities
  *  towards the final configuration, while image capture, peak detection and triangulation is performed at the same time.
@@ -9,90 +8,121 @@
  *  an ESM tracker is also initialized, and used for estimating the motion of the mobile vehicle.
  *  Finishes on !ros::ok(), when a click is done in the image (if drawing enabled), or when the final arm configuration is reached.
  */
-int Reconstruction3DAction::doAction() {
-	if (!offline_ && robot_)  {
-		//Move the arm to the initial position
-		std::cout<<vp_scan_initial_posture_<<std::endl;
-		robot_->setJointValues(vp_scan_initial_posture_, true);
-	}
-	int cont=0;
-	//The main loop. Acquire image, track template, detect peaks, estimate motion, and reconstruction, while
-	//controlling the arm through the scan:
-	vpHomogeneousMatrix eMl;
-	done_=false;
-	boost::thread threads[rec_.size()];
-	for(int i=0; i<rec_.size(); i++){
-		threads[i]=this->createThread(rec_[i]);
-	}
-	sleep(1);
-	while (ros::ok() && !done_) {
-		ros::spinOnce();
+int Reconstruction3DAction::doAction()
+{
 
-		//Update perceptions
-		if (!fixed_base_ && tracker_ && mest_) {
-			tracker_->perceive();
-			mest_->perceive();
-		}
+  if (!offline_ && robot_)
+  {
+    //Move the arm to the initial position
+    //std::cout<<vp_scan_initial_posture_<<std::endl;
 
-		draw();
+	  vpColVector current_joints;
+	  robot_->getJointValues(current_joints);
+	  vp_scan_initial_posture_[4]=current_joints[4];
+	  while((vp_scan_initial_posture_-current_joints).euclideanNorm()>0.07 && ros::ok()){
+		  std::cout<<"Error: "<<(vp_scan_initial_posture_-current_joints).euclideanNorm()<<std::endl;
+		  robot_->setJointVelocity((vp_scan_initial_posture_-current_joints)*10.0);
+		  robot_->getJointValues(current_joints);
+		  ros::spinOnce();
+	  }
 
-		/*for(int i=0; i<rec_.size(); i++){
-			rec_[i]->perceive();
+    //robot_->setJointValues(vp_scan_initial_posture_, true, 0.5);
+  }
+  int cont = 0;
+  //The main loop. Acquire image, track template, detect peaks, estimate motion, and reconstruction, while
+  //controlling the arm through the scan:
+  vpHomogeneousMatrix eMl;
+  done_ = false;
+  //boost::thread threads[rec_.size()];
+  /*for(int i=0; i<rec_.size(); i++){
+   threads[i]=this->createThread(rec_[i]);
+   }*/
+  //sleep(1);
+  while (ros::ok() && !done_)
+  {
+    ros::spinOnce();
+    //Update perceptions
+    if (!fixed_base_ && tracker_ && mest_)
+    {
+      tracker_->perceive();
+      mest_->perceive();
+    }
+    draw();
+    for (int i = 0; i < rec_.size(); i++)
+    {
+      rec_[i]->perceive();
 
-		}*/
+    }
 
-		//if (drawing_enabled && vpDisplay::getClick(*Idraw_, false)) done=true;
+    //if (drawing_enabled && vpDisplay::getClick(*Idraw_, false)) done=true;
 
-		//Make the scan motion: Send the arm to the final joint position with a suitable joint velocity
-		if (!offline_) {
-			double scale=1;
-			vpColVector q;
-			robot_->getJointValues(q);
-			if ((vp_scan_final_posture_ - q).infinityNorm()>max_joint_velocity_) {
-				//scale to the maximum allowed velocity
-				scale=max_joint_velocity_/(vp_scan_final_posture_-q).infinityNorm();
-			}
-			if ((vp_scan_final_posture_ - q).infinityNorm() < position_tolerance_) done_=true;
-			robot_->setJointVelocity((vp_scan_final_posture_-q)*scale);
-		}
+    //Make the scan motion: Send the arm to the final joint position with a suitable joint velocity
+    if (!offline_)
+    {
+      double scale = 1;
+      vpColVector q;
+      robot_->getJointValues(q);
+      if ((vp_scan_final_posture_ - q).infinityNorm() > max_joint_velocity_)
+      {
+        //scale to the maximum allowed velocity
+        scale = max_joint_velocity_ / (vp_scan_final_posture_ - q).infinityNorm();
+      }
+      if ((vp_scan_final_posture_ - q).infinityNorm() < position_tolerance_)
+        done_ = true;
+      robot_->setJointVelocity((vp_scan_final_posture_ - q) * scale);
+    }
 
-		cont++;
-	}
-	std::cout<<"Cont="<<cont<<std::endl;
-	for(int i=0; i<rec_.size(); i++){
-		threads[i].join();
-	}
+    cont++;
+  }
+  std::cout << "Cont=" << cont << std::endl;
+  /*for(int i=0; i<rec_.size(); i++){
+   threads[i].join();
+   }*/
 
-	return SUCCESS;
+  return SUCCESS;
 }
 
-void Reconstruction3DAction::draw() {
-	if(drawing_enabled){
-		for(int i=0; i<rec_.size(); i++){
-			cv::Mat image;
-			vpImage<vpRGBa> Ic;
-			rec_[i]->getPeakDetector()->getGrabber()->acquire(Ic);
-			vpImageConvert::convert(Ic, image);
-			cv::namedWindow("camera"+boost::lexical_cast<std::string>(i), cv::WINDOW_AUTOSIZE);
-			cv::line(image,cv::Point(0, rec_[i]->getPeakDetector()->getLimits()[0]),
-				           cv::Point(Ic.getCols()-1, rec_[i]->getPeakDetector()->getLimits()[0]),
-					       cv::Scalar(255,0,0));
-			cv::line(image,cv::Point(0, rec_[i]->getPeakDetector()->getLimits()[1]),
-							           cv::Point(Ic.getCols()-1, rec_[i]->getPeakDetector()->getLimits()[1]),
-							           cv::Scalar(255,0,0));
-			for(unsigned int j=0; j<rec_[i]->getPeakDetector()->points.size(); j++){
+void Reconstruction3DAction::draw()
+{
+  if (drawing_enabled)
+  {
+    for (int i = 0; i < rec_.size(); i++)
+    {
+      cv::Mat image;
+      vpImage<vpRGBa> Ic;
+      rec_[i]->getPeakDetector()->getGrabber()->acquire(Ic);
+      vpImageConvert::convert(Ic, image);
+      cv::namedWindow("camera" + boost::lexical_cast<std::string>(i), cv::WINDOW_AUTOSIZE);
+      cv::line(image, cv::Point(0, rec_[i]->getPeakDetector()->getLimits()[0]),
+               cv::Point(Ic.getCols() - 1, rec_[i]->getPeakDetector()->getLimits()[0]), cv::Scalar(255, 0, 0));
+      cv::line(image, cv::Point(0, rec_[i]->getPeakDetector()->getLimits()[1]),
+               cv::Point(Ic.getCols() - 1, rec_[i]->getPeakDetector()->getLimits()[1]), cv::Scalar(255, 0, 0));
+      for (unsigned int j = 0; j < rec_[i]->getPeakDetector()->points.size(); j++)
+      {
 
-				//	std::cout<<rec_[i]->getPeakDetector()->points[i][1]<<"----"<<rec_[i]->getPeakDetector()->points[i][0]<<std::endl;
-				cv::Vec3b & pixel=image.at<cv::Vec3b>(rec_[i]->getPeakDetector()->points[j][0], rec_[i]->getPeakDetector()->points[j][1]);
-				pixel[0]=0;
-				pixel[1]=0;
-				pixel[2]=255;
+        //	std::cout<<rec_[i]->getPeakDetector()->points[i][1]<<"----"<<rec_[i]->getPeakDetector()->points[i][0]<<std::endl;
+        cv::Vec3b & pixel = image.at<cv::Vec3b>(rec_[i]->getPeakDetector()->points[j][0],
+                                                rec_[i]->getPeakDetector()->points[j][1]);
+        pixel[0] = 0;
+        pixel[1] = 0;
+        pixel[2] = 255;
 
-			}
-			cv::imshow("camera"+boost::lexical_cast<std::string>(i), image);
-			cv::waitKey(5);
-		}
+      }
+      cv::imshow("camera" + boost::lexical_cast<std::string>(i), image);
+      cv::waitKey(5);
+    }
 
-	}
+  }
 
+}
+void Reconstruction3DAction::benchmarkCallback(const std_msgs::String::ConstPtr &msg)
+{
+  if (msg->data != "")
+  {
+    for (int i = 0; i < rec_.size(); i++)
+    {
+      rec_[i]->points3d.clear();
+    }
+    this->doAction();
+  }
 }
